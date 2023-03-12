dbc475cea0596df0980dd3c92ab84d351f05a68b
Nischal Shakya <nischalshakya2015@gmail.com>
2023-03-12 09:03:16 +0545

    chore: perform minor packages upgrades
    - Upgrade `@types/node` to `18.15.0`
    - Update `node` engine version to `>=18`
    - Upgrade `pnpm` to `7.29.1`
    - Add location alias to `vite.config.ts` and `tsconfig.json`

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,8 @@ export default defineConfig((configEnv) => {
         pages: resolve(__dirname, 'src', 'pages'),
         components: resolve(__dirname, 'src', 'components'),
         hooks: resolve(__dirname, 'src', 'hooks'),
+        lib: resolve(__dirname, 'src', 'lib'),
+        services: resolve(__dirname, 'src', 'services'),
       },
     },
     css: {
