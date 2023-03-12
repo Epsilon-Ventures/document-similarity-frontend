538f45e1451d738cbfc14c1ceb0bbfcf8960b457
Nischal Shakya <nischalshakya2015@gmail.com>
2023-03-12 09:46:40 +0545

    refactor: implement axios interface

---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as uploadFile } from './upload-file';
+
+export * from './post-question';
--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig((configEnv) => {
         hooks: resolve(__dirname, 'src', 'hooks'),
         lib: resolve(__dirname, 'src', 'lib'),
         services: resolve(__dirname, 'src', 'services'),
+        data: resolve(__dirname, 'src', 'data'),
       },
     },
     css: {
