ed0be806acfd5e129ae6a44cc61c4bf78ea5ad2d
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2023-05-02 01:12:51 +0545

    feat: add ui design for top-two-question

--- src/pages/top-two-question-response/molecules/top-two-question-response.tsx
+++ src/pages/top-two-question-response/molecules/top-two-question-response.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { nanoid } from 'nanoid';
-import styles from './top-two-question-response.module.css';
+import classes from './top-two-question-response.module.css';
 
 interface QuestionInterface {
   question: string[];
@@ -32,24 +32,32 @@ function TopTwoQuestionResponse() {
   }, []);
 
   return (
-    <div className={`${styles.file_question_container}`}>
-      <h2>{state.selectSubject}</h2>
-      <div className="flex flex-col gap-2">
-        {/* eslint-disable-next-line operator-linebreak */}
-        {inputFiles !== null &&
-          fileResponses.map((responses, index) => (
-            <div key={nanoid()}>
-              <h3>{inputFiles[index]}</h3>
-              <div className="flex flex-col gap-1">
-                {responses.map((response) => (
-                  <div className="grid grid-cols-6">
-                    <p>{response.question}</p>
-                    <p>{response.sim_score}</p>
-                  </div>
-                ))}
+    <div className={classes.top_two_question_section}>
+      <div className={classes.container}>
+        <h2 className="mb-2 text-source-primary">
+          {state.selectSubject.toUpperCase()}
+        </h2>
+        <div className="flex flex-col gap-2">
+          {/* eslint-disable-next-line operator-linebreak */}
+          {inputFiles !== null &&
+            fileResponses.map((responses, index) => (
+              <div className={classes.question_card} key={nanoid()}>
+                <h3 className={classes.my_question}>{inputFiles[index]}</h3>
+                <div className={classes.response_question_card}>
+                  {responses.map((response) => (
+                    <div className="grid grid-cols-6 items-center gap-2 ">
+                      <p className="col-span-5 leading-10 ">
+                        {response.question}
+                      </p>
+                      <p className="justify-self-end text-[2rem] text-source-primary">
+                        {response.sim_score}
+                      </p>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
     </div>
   );
